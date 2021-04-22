ca33a5a5f290912843c273127e632977052370f6
Rebecca Choo <rnwldms@gmail.com>
2021-04-22 14:07:59 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,13 +4,68 @@ import Board from "./components/Board";
 import "./App.css";
 
 class App extends Component {
-	state = { answer: ["a", "a", "", "", "", "", "", "", ""] };
+	state = {
+		answer: ["", "", "", "", "", "", "", "", ""],
+		gameDone: false,
+		messageWin: "Good Job! You Won The Game!",
+		messageFail: "You Failed. Try Again?",
+		winOrFail: false,
+	};
+
+	clicked = (index) => {
+		if (this.state.answer[index].length === 0) {
+			this.setState(
+				(prevState) => {
+					const list = prevState.answer.map((as, ind) => {
+						return ind === index ? "X" : as;
+					});
+
+					return { answer: list };
+				},
+				() => this.computerTurn()
+			);
+		}
+	};
+	reset = () => {
+		this.setState({
+			answer: ["", "", "", "", "", "", "", "", ""],
+			gameDone: false,
+			winOrFail: false,
+		});
+	};
+	computerTurn = () => {
+		const isFull = (currentValue) => currentValue.length > 0;
+		const allFull = this.state.answer.every(isFull);
+		if (allFull) {
+			this.setState({ gameDone: true });
+			return;
+		}
+
+		let randomIndex = Math.floor(Math.random() * this.state.answer.length);
+		this.state.answer[randomIndex] === ""
+			? this.setState((state) => {
+					const list = state.answer.map((as, ind) => {
+						return ind === randomIndex ? "0" : as;
+					});
+					return { answer: list };
+			  })
+			: setTimeout(() => {
+					this.computerTurn();
+			  }, 100);
+	};
 	render = () => {
 		return (
 			<div className="App">
 				<h1>Tic Tac Toe Game</h1>
 				<div className="exp">You are X. Your opponent is O.</div>
-				<Board answer={this.state.answer} />
+				<button onClick={this.reset}>Play again</button>
+				<br />
+				<br />
+				<Board
+					gameDone={this.state.gameDone}
+					answer={this.state.answer}
+					clicked={this.clicked}
+				/>
 			</div>
 		);
 	};
--- src/components/Board.js
+++ src/components/Board.js
@@ -2,13 +2,24 @@ import React from "react";
 import "./Board.css";
 
 const Board = (props) => {
-	console.log(props.answer);
+	const itemClicked = (index) => {
+		props.clicked(index);
+	};
+
 	return (
 		<div className="Board">
-			<div className="ItemWrap">
+			<div
+				className="ItemWrap"
+				style={{ backgroundColor: props.gameDone ? "red" : "white" }}
+			>
 				{props.answer.map((list, index) => {
 					return (
-						<div key={index} className="Item">
+						<div
+							key={index}
+							className="Item"
+							onClick={() => itemClicked(index)}
+							style={{ pointerEvents: props.gameDone ? "none" : "" }}
+						>
 							{list}
 						</div>
 					);
